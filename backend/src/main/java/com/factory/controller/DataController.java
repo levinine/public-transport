176232d81d61bc31ce76bb4d4b07987e4e261b8a
Nikola Zeljkovic <nikola.zeljkovic@codecentric.de>
2018-11-24 11:53:37 +0100

    Remove unused method

--- backend/src/main/java/com/factory/controller/DataController.java
+++ backend/src/main/java/com/factory/controller/DataController.java
@@ -1,7 +1,6 @@
 package com.factory.controller;
 
 import com.factory.common.api.RestApiEndpoints;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(RestApiEndpoints.DATA)
 public class DataController {
 
-    @GetMapping(value = "/test")
-    public String test() {
-        return "Hello world";
     }
 }
