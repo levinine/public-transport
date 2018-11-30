9ca38ffa8de3f544449bac9251e1199c4db0c50c
SvetislavMarjanovic <svetaftn93@gmail.com>
2018-11-30 10:45:43 +0100

    change url and remove request header configuration

--- frontend/src/app/routes.service.ts
+++ frontend/src/app/routes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
-const API_URL = 'http://localhost:8080/api/v1'; // izbrisi localhost kad pravis jar
+const API_URL = '/api/v1'; 
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +12,6 @@ export class RoutesService {
   constructor(private http: HttpClient) { }
 
   getRoutes(start: String, end: String, date: String):  Observable<any> {
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      // @ts-ignore
-      let reverseStart;
-      let reverseEnd;
-
-      // @ts-ignore
-      return this.http.get(`${API_URL}/routes?start=${start}&end=${end}&date=${date}`, headers);
+      return this.http.get(`${API_URL}/routes?start=${start}&end=${end}&date=${date}`);
   }
 }
