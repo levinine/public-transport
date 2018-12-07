0f85b2061db48db4f4e9b7b01acaff402dd7ba7a
Milos Jovanovic <m.jovanovic@levi9.com>
2018-12-07 14:39:12 +0100

    Added integration test examples

---
+++ backend/src/test/java/com/factory/ContextTest.java
@@ -0,0 +1,16 @@
+package com.factory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BackendApplication.class)
+public class ContextTest {
+
+	@Test
+	public void testApplictionContextStarts() {
+		System.out.println("Application context started");
+	}
+}
